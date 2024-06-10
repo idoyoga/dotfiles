f19f18f27e8c78c91167085e651195b51888003b
Daniel <157289799+idoyoga@users.noreply.github.com>
2024-06-10 21:56:11 +0200

    Add files via upload

---
+++ config.fish
@@ -0,0 +1,20 @@
+if status is-interactive
+alias cc='cc -Werror -Wall -Wextra'
+alias nor='norminette -R CheckForbiddenSourceHeader'
+alias c='clear'
+alias gst='git status'
+alias gp='git push'
+alias gl='git log'
+alias gc='git commit -m'
+alias ga='git add .'
+alias gf='git fetch'
+alias gpl='git pull'
+alias gcl='git clone'
+alias gch='git checkout'
+alias gb='git branch'
+alias gr='git remote'
+alias v='vim'
+alias francinette="$HOME"/francinette/tester.sh
+alias paco="$HOME"/francinette/tester.sh
+# Commands to run in interactive sessions can go here
+end
