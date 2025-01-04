22ecbf85ad2a1ed16559fc6e5e4a692f58459011
dp <dplotzl@student.42vienna.com>
2025-01-04 17:31:43 +0100

    Keybind for Copilot complete changed from Tab to Ctrl-J

--- nvim/lua/core/lazy.lua
+++ nvim/lua/core/lazy.lua
@@ -118,7 +118,11 @@ require("lazy").setup({
                 ['cpp'] = true, -- Enable for C++
             }
 			  -- Make sure Tab is free for Copilot to use
-			vim.g.copilot_no_tab_map = false
+			vim.keymap.set('i', '<C-J>', 'copilot#Accept("\\<CR>")', {
+			  expr = true,
+			  replace_keycodes = false
+			})
+			vim.g.copilot_no_tab_map = true
         end,
     },
 })
